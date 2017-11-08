2445b4a4e389e714fe6b967cc413fd16e0d15fc3
Vladimir Vakhrushev <lenta-b@yandex.ru>
2017-11-08 22:42:40 +0300

    First HW

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
         <footer className="App-footer">
-          <p className="App-title"> loftschool </p>
+          <p className="App-title">Loftschool</p>
         </footer>
       </div>
     );
